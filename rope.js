09aa6b43724e909873af5f4afcdd533756d92e71
MoumitaRoy14 <76441152+MoumitaRoy14@users.noreply.github.com>
2025-10-30 19:33:11 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,53 @@
+
+  /*  
+    drawVertices(vertices) 
+    {
+      beginShape();
+      fill('#FFF717')
+      noStroke();
+      
+      for (let i = 0; i < vertices.length; i++) 
+      {
+       vertex(vertices[i].x, vertices[i].y);
+       }
+      endShape(CLOSE);
+   }
+
+   showConstraints(constraints) 
+   {
+     if(constraints!=null)
+     {
+    for (let i = 0; i < constraints.length; i++) {
+      this.drawConstraint(constraints[i]);
+    }
+  }
+  }
+  
+  drawConstraint(constraint) {
+    if(constraint!=null)
+      {
+    const offsetA = constraint.pointA;
+    let posA = {x:0, y:0};
+    if (constraint.bodyA) {
+      posA = constraint.bodyA.position;
+    }
+    const offsetB = constraint.pointB;
+    let posB = {x:0, y:0};
+    if (constraint.bodyB) {
+      posB = constraint.bodyB.position;
+    }
+    push()
+    strokeWeight(4);
+    stroke(255);
+    line(
+      posA.x + offsetA.x,
+      posA.y + offsetA.y,
+      posB.x + offsetB.x,
+      posB.y + offsetB.y
+    );
+    pop();
+      }
+  }*/
+  
+    
+  
